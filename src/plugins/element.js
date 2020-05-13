13be176ed2f0d3c039d794f484c025644113012f
macycle <1124942097@qq.com>
2020-05-13 15:06:44 +0800

    完成添加功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { Button,Form,FormItem,Input,Message,Container,Header,Aside,Main} from 'element-ui'
 import {Menu,Submenu,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col} from 'element-ui'
 import {Table,TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox} from 'element-ui'
-import {Tag,Tree,Select,Option} from 'element-ui'
+import {Tag,Tree,Select,Option,Cascader} from 'element-ui'
 
 
 //全局注册
@@ -32,7 +32,7 @@ Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
-
+Vue.use(Cascader)
 
 
 Vue.prototype.$message=Message //将message组件挂载到实例原型上，这样所有的都可以用到这个组件
